806c3f17d75e3f19612eb524cf6753635362c847
Dmitriy Kochnev <dima_koch_dima@mail.ru>
2024-07-08 14:41:17 +0300

    Эхо бот

---
+++ main.py
@@ -0,0 +1,28 @@
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings, logging
+
+logging.basicConfig(filename='bot.log',level=logging.INFO)
+
+def greet_user(update,context):
+    print("Бот начал работу!")
+    update.message.reply_text("Здравствуй пользователь")
+
+def talk_to_me(update,context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
+def main():
+    mybot = Updater(token=settings.TOKEN,use_context=True)
+    
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start",greet_user))
+    dp.add_handler(MessageHandler(Filters.text,talk_to_me))
+    
+    logging.info("Бот стартовал!")
+    
+    mybot.start_polling()
+    mybot.idle()
+    
+if __name__ == '__main__':
+    main()
